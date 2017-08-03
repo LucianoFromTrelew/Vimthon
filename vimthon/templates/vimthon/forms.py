9883f6b6b73dbac0a466220fd94abf8993c6e161
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-08-03 16:35:51 -0300

    formulario

--- vimthon/templates/vimthon/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class RegexForm(forms.ModelForm):
-
-	regex_validator = RegexValidator(regex= r'')
-	text = TextField(required=True)
-	regex = TextField(required=True, validators=[
-		RegexValidator('^:\/s\/\w+\/\w+\/g', )])
