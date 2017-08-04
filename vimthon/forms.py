5fca6568d26bfb06ff406284aac8a99ffc341126
papablo <pabloatm980@gmail.com>
2017-08-03 23:46:47 -0300

    prueba del form

--- vimthon/forms.py
+++ vimthon/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class RegexForm(forms.Form):
 
     text = forms.CharField(widget=forms.Textarea, required=True)
-    regex = forms.CharField(required=True) 
+    regex = forms.CharField(required=True)
         #RegexValidator('^:\/s\/\w+\/\w+\/g', )
