ba41e2c8c5340b4f13d5dd074d37a698c6e3037f
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-08-04 00:54:50 -0300

    primer boceto de la expresion regular que necesitamos

--- vimthon/forms.py
+++ vimthon/forms.py
@@ -3,4 +3,21 @@ from django import forms
 class RegexForm(forms.Form):
 
     text = forms.CharField(widget=forms.Textarea, required=True)
-    regex = forms.CharField(required=True) 
+    regex = forms.CharField(required=True)
+
+    """r':(%|\d+,\d+)?s/(\w+)?/(\w+)?/g', ':1,2s/foo//g'
+
+	esa expresion regular matchea bien con el %, con dos numeros separados por coma, o con nada (antes de la 's')
+	matchea con la palabra a buscar
+	matchea con la palabra a reemplazar
+
+	faltaria hacer que pueda matchear con expresiones regulares tambien o algo mas complejo
+	en el campo que va lo que se busca
+
+	por ejemplo, si tenemos
+
+	:%s/foo/bar/g
+
+	que el 'foo', pueda ser una expresion regular. Asi como esta ahora la exp, solo busca palabras
+	
+    """
