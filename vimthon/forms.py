f9b18c26b647abd2bfab0a4cc57bf0da69399d79
papablo <pabloatm980@gmail.com>
2017-08-03 22:17:53 -0300

    views and forms

--- vimthon/forms.py
+++ vimthon/forms.py
@@ -1,1 +1,7 @@
-f
+from django import forms
+
+class RegexForm(forms.Form):
+
+    text = forms.CharField(widget=forms.Textarea, required=True)
+    regex = forms.CharField(required=True) 
+        #RegexValidator('^:\/s\/\w+\/\w+\/g', )
--- vimthon/views.py
+++ vimthon/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from . import forms
 
-def main(request): 
-	return render(request, 'vimthon/main.html', {})
+def main(request):
+        form = forms.RegexForm()
+        return render(request, 'vimthon/main.html', {'form':form})
 # Create your views here.
 
