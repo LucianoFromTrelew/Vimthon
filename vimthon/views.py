e7f65ed424b8ebfd7fa9758234b3e73671d5fa4e
papablo <pabloatm980@gmail.com>
2017-08-07 12:22:23 -0300

    variable global para conseguir el cursor

--- vimthon/views.py
+++ vimthon/views.py
@@ -10,27 +10,30 @@ import lorem
 cursor = -1
 
 def main(request):
-	if request.method == 'POST' :
-		form = forms.RegexForm(request.POST)
-		if form.is_valid():
-			text = form.cleaned_data['text']
-			regex = form.cleaned_data['regex']
-			match = re.search(r':(%|\d+,\d+)?s/(\w+)?/(\w+)?/g$', regex)
-			search = re.compile(match.group(2))
-			text = re.sub(search, match.group(3), text)
-			form = forms.RegexForm()
-			form.fields['text'].initial=text
-			#form.text = formsdjango.CharField(widget=formsdjango.Textarea, initial=text)
-			return render(request, 'vimthon/main.html', {'form':form})
+    print('El cursor está en {}'.format(cursor))
+    if request.method == 'POST' :
+        form = forms.RegexForm(request.POST)
+        if form.is_valid():
+                text = form.cleaned_data['text']
+                regex = form.cleaned_data['regex']
+                match = re.search(r':(%|\d+,\d+)?s/(\w+)?/(\w+)?/g$', regex)
+                search = re.compile(match.group(2))
+                text = re.sub(search, match.group(3), text)
+                form = forms.RegexForm()
+                form.fields['text'].initial=text
+                #form.text = formsdjango.CharField(widget=formsdjango.Textarea, initial=text)
+                return render(request, 'vimthon/main.html', {'form':form})
 
-		return render(request, 'vimthon/main.html', {'form':form})
+        return render(request, 'vimthon/main.html', {'form':form})
 
-	else:
-		form = forms.RegexForm()
-		#form.fields['text'].initial = lorem.paragraph
-		return render(request, 'vimthon/main.html', {'form':form})
+    else:
+        form = forms.RegexForm()
+        #form.fields['text'].initial = lorem.paragraph
+        return render(request, 'vimthon/main.html', {'form':form})
+
 @csrf_exempt
 def set_cursor(request):
+    global cursor 
     cursor = request.GET.get('cursor', None)
     
     if cursor is not None:
