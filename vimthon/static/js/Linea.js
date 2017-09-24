50a498b2824df9a23655c4d74c3ddb6beaa4d594
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-09-24 20:05:16 -0300

    ordenando archivos .js

---
+++ vimthon/static/js/Linea.js
@@ -0,0 +1,125 @@
+
+/*
+ *  Lo que se me ocurre es armar un arreglo con los nombres de los miembros del JSON que nos devuelve la gramatica
+ *  ir recorriendo el JSON con esos nombres, e ir envolviendo acorde
+ *
+ *  La que podria ser es armar un diccionario que tenga el nombre de miembro, y el color, asi pintamos segun el miembro
+ *
+ *  Habría que ver cómo s e pueden manejar las situaciones en las que se quiere acceder a un miembro de un objeto que no tiene dicho miembro
+ *  Osea, cómo atajar la excepción "AttributeError"
+ * 
+ * 
+ *  otra cosa puede ser, recorrer el json, e ir armando una cadena con todo lo parseado, ya coloreado
+ * */
+
+
+ /*para definir las clases*
+
+ 
+ */
+class Linea {
+    constructor(colores, linea){
+      this._colores = colores
+      this._linea = linea
+    }
+  
+    get colores(){
+      return this._colores
+    }
+  
+    get linea(){
+      return this._linea
+    }
+  
+    colorear(){
+      throw "MÉTODO ABSTRACTO"
+    }
+  
+    spanner(color, texto){
+      return "<span style=\"color: {0}\">{1}</span>".format(color, texto) 
+    }
+  }
+  
+  class LineaAsignacion extends Linea {
+  
+    constructor(linea){
+      var colores = ["red", "blue"]
+      super(colores, linea)
+    }
+  
+    colorear(){
+      console.log(this.linea)
+      return "{0} {1} {2}\n".format(
+      this.spanner(this.colores[0], this.linea.izq.VARIABLE),
+      this.linea.igual, 
+      this.spanner(this.colores[1], this.linea.der["0"].NUMERO))
+  
+    }
+  }
+  
+  class LineaExpresion extends Linea {
+  
+    constructor(linea){
+      var colores = ["green", "red"]
+      super(colores, linea)
+    }
+  
+    colorear(){
+      return "{0} {1} {2}".format(
+        this.spanner(this.colores[0], this.linea.op_izquierdo["0"].VARIABLE),
+        this.spanner(this.colores[1], this.linea.operador["0"]), 
+        this.spanner(this.colores[0], this.linea.op_derecho["0"].NUMERO))  
+    }
+  }
+  
+  class LineaWhile extends Linea {
+    
+    constructor(linea){
+      var colores = ["blue", "red"]
+      super(colores, linea)
+    }
+  
+    colorear(){
+      return "{0} {1}{2}\n".format(
+        this.spanner(this.colores[0], this.linea.while),
+        new LineaExpresion(this.linea.condicion).colorear(),
+        this.spanner(this.colores[1], this.linea.dospuntos)
+      )
+    }
+  
+  }
+  class LineaFactory {
+   
+    static crear_linea(linea){
+      
+      const tipos_linea = ["operador", "while", "igual"]
+      
+      var claves_linea = Object.keys(linea)
+  
+      /* Cada tipo de linea (asignacion, bucle, expresion)
+      tiene por lo menos una clave en el JSON que obtenemos de la produccion
+      que va a ser unica (bah, todas son unicas, no hay repetidas)
+      preguntamos por alguna de esas claves, y en base a la condicion,
+      creamos un objeto de la clase correspondiente */
+  
+      /**
+       * TODO: Crear la clase Linea y las subclases
+       * 
+       * LineaAsignacion
+       * LineaExpresion
+       * LineaWhile
+       * 
+       * la clase Linea va a tener un atributo que viene a ser el color
+       * y el metodo colorear() que devuelve una cadena con el texto de la linea
+       * envuelto en los spans acordemente
+       */
+      if (/operador/i.test(claves_linea)){
+        return new LineaExpresion(linea)
+      }else if (/while/i.test(claves_linea)){
+        return new LineaWhile(linea)
+      }else{
+        return new LineaAsignacion(linea)
+      }
+    }
+  
+  }  
--- vimthon/static/js/index.js
+++ vimthon/static/js/index.js
@@ -1,18 +1,6 @@
 // Document ready stuff
 const codigo = '#editor'
 
-if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
-}
-
 $(document).ready(function(){
     // cuando submiteo el form
     $('#text_form').submit(function(event){
@@ -34,149 +22,20 @@ $(document).ready(function(){
 });
 
 
-
-
-/*
- *  Lo que se me ocurre es armar un arreglo con los nombres de los miembros del JSON que nos devuelve la gramatica
- *  ir recorriendo el JSON con esos nombres, e ir envolviendo acorde
- *
- *  La que podria ser es armar un diccionario que tenga el nombre de miembro, y el color, asi pintamos segun el miembro
- *
- *  Habría que ver cómo s e pueden manejar las situaciones en las que se quiere acceder a un miembro de un objeto que no tiene dicho miembro
- *  Osea, cómo atajar la excepción "AttributeError"
- * 
- * 
- *  otra cosa puede ser, recorrer el json, e ir armando una cadena con todo lo parseado, ya coloreado
- * */
-
-
- /*para definir las clases*
-
- 
- */
-class Linea {
-  constructor(colores, linea){
-    this._colores = colores
-    this._linea = linea
-  }
-
-  get colores(){
-    return this._colores
-  }
-
-  get linea(){
-    return this._linea
-  }
-
-  colorear(){
-    throw "MÉTODO ABSTRACTO"
-  }
-
-  spanner(color, texto){
-    return "<span style=\"color: {0}\">{1}</span>".format(color, texto) 
-  }
-}
-
-class LineaAsignacion extends Linea {
-
-  constructor(linea){
-    var colores = ["red", "blue"]
-    super(colores, linea)
-  }
-
-  colorear(){
-    return "{0} {1} {2}\n".format(
-    this.spanner(this.colores[0], this.linea.izq.VARIABLE),
-    this.linea.igual, 
-    this.spanner(this.colores[1], this.linea.der["0"].NUMERO))
-
-  }
-}
-
-class LineaExpresion extends Linea {
-
-  constructor(linea){
-    var colores = ["green", "red"]
-    super(colores, linea)
-  }
-
-  colorear(){
-    return "{0} {1} {2}".format(
-      this.spanner(this.colores[0], this.linea.op_izquierdo["0"].VARIABLE),
-      this.spanner(this.colores[1], this.linea.operador["0"]), 
-      this.spanner(this.colores[0], this.linea.op_derecho["0"].NUMERO))  
-  }
-}
-
-class LineaWhile extends Linea {
-  
-  constructor(linea){
-    var colores = ["blue", "red"]
-    super(colores, linea)
-  }
-
-  colorear(){
-    return "{0} {1}{2}\n".format(
-      this.spanner(this.colores[0], this.linea.while),
-      new LineaExpresion(this.linea.condicion).colorear(),
-      this.spanner(this.colores[1], this.linea.dospuntos)
-    )
-  }
-
-}
-class LineaFactory {
- 
-  static crear_linea(linea){
-    
-    const tipos_linea = ["operador", "while", "igual"]
-    
-    var claves_linea = Object.keys(linea)
-
-    /* Cada tipo de linea (asignacion, bucle, expresion)
-    tiene por lo menos una clave en el JSON que obtenemos de la produccion
-    que va a ser unica (bah, todas son unicas, no hay repetidas)
-    preguntamos por alguna de esas claves, y en base a la condicion,
-    creamos un objeto de la clase correspondiente */
-
-    /**
-     * TODO: Crear la clase Linea y las subclases
-     * 
-     * LineaAsignacion
-     * LineaExpresion
-     * LineaWhile
-     * 
-     * la clase Linea va a tener un atributo que viene a ser el color
-     * y el metodo colorear() que devuelve una cadena con el texto de la linea
-     * envuelto en los spans acordemente
-     */
-    if (/operador/i.test(claves_linea)){
-      return new LineaExpresion(linea)
-    }else if (/while/i.test(claves_linea)){
-      return new LineaWhile(linea)
-    }else{
-      return new LineaAsignacion(linea)
-    }
-  }
-
-}
-
 function update() {
   var p = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
   var texto = $(codigo).html().replace(/<br>/g,'')
   .replace(/<div>/g,'\n').replace(/<\/div>/g, '')
   .replace(/&lt;/g, '<').replace(/&gt;/g, '>')
   .replace(/<code>/g,'').replace(/<\/code>/g,'')
-  // .replace(/<span.*>/g, '').replace(/<\/span>/g,'')
 
   console.log(texto)
   p.feed(texto)
 
   try {
     var cuerpo = p.results[0].cuerpo
-    // console.log(cuerpo)
   } catch (error){}
   
-  // console.log(texto)
   
   for (var l in cuerpo) {
     try {
@@ -190,11 +49,7 @@ function update() {
       $("#code_editor")
       .html(texto.concat(LineaFactory
       .crear_linea(linea).colorear()))
-      
-      // texto.concat(LineaFactory.crear_linea(linea).colorear())
-      
-      console.log(cuerpo[l].linea)
-
+    
     } catch (error) {
       continue
     }
