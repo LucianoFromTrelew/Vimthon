cb36b297bf9a6bdce3ba0a6a504ae73b66dffae8
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-09-25 12:55:05 -0300

    sacando consoleslogsss

--- vimthon/static/js/index.js
+++ vimthon/static/js/index.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     // cuando submiteo el form
     $('#text_form').submit(function(event){
         var position = cursor();
-        console.log('j')
         $.ajax({
             type: "POST",
             url: '/cursor',
@@ -29,7 +28,6 @@ function update() {
   .replace(/&lt;/g, '<').replace(/&gt;/g, '>')
   .replace(/<code>/g,'').replace(/<\/code>/g,'')
 
-  console.log(texto)
   p.feed(texto)
 
   try {
@@ -70,29 +68,3 @@ function cursor(){
     console.log(end)
     return (start==end) ? start : -1;
 }
-
-
-// function getCaretPosition(editableDiv) {
-// var caretPos = 0,
-//     sel, range;
-//   if (window.getSelection) {
-//     sel = window.getSelection();
-//     if (sel.rangeCount) {
-//       range = sel.getRangeAt(0);
-//       if (range.commonAncestorContainer.parentNode == editableDiv) {
-//         caretPos = range.endOffset;
-//       }
-//     }
-//   } else if (document.selection && document.selection.createRange) {
-//     range = document.selection.createRange();
-//     if (range.parentElement() == editableDiv) {
-//       var tempEl = document.createElement("span");
-//       editableDiv.insertBefore(tempEl, editableDiv.firstChild);
-//       var tempRange = range.duplicate();
-//       tempRange.moveToElementText(tempEl);
-//       tempRange.setEndPoint("EndToEnd", range);
-//       caretPos = tempRange.text.length;
-//     }
-//   }
-//   return caretPos;
-// }
